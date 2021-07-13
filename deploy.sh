f2fc9589fdcd0f55499aa55957dc68458ee4103c
Rene Rivera <grafikrobot@gmail.com>
2021-07-12 20:36:09 -0500

    Use rsync for deply for more complex site structure.
    Using rsync allows for keeping a better sync between the server and dev
    trees. Specifically it will delete old files and copy over the directory
    structure.
    
    This also moves the local build dir from "dist" to "barbarian.bfgroup.xyz"
    to mirror the server name.

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,16 @@
 # (See accompanying file LICENSE.txt or copy at
 # http://www.boost.org/LICENSE_1_0.txt)
 
+# Clean build the site.
 npm run build
-b2 src//html
-scp dist/*.js dist/*.css dist/*.png dist/*.html dist/*.ico package.json bfgbarbarian@barbarian.bfgroup.xyz:/home/bfgbarbarian/barbarian.bfgroup.xyz/
+b2 src/website/static//html
+
+# Deploy the built "dist" tree.
+rsync -v -c -r --delete -h barbarian.bfgroup.xyz bfgbarbarian@barbarian.bfgroup.xyz:/home/bfgbarbarian/
+rsync -v -c -r --delete -h src/website/.htaccess package.json bfgbarbarian@barbarian.bfgroup.xyz:/home/bfgbarbarian/barbarian.bfgroup.xyz/
+
+# Deploy the out-of-root auth creds for the database.
 scp .dbconf*.js bfgbarbarian@barbarian.bfgroup.xyz:/home/bfgbarbarian
-ssh bfgbarbarian@barbarian.bfgroup.xyz "cd /home/bfgbarbarian/barbarian.bfgroup.xyz && npm install && touch tmp/restart.txt"
+
+# Update the node modules and reboot the server to match.
+ssh bfgbarbarian@barbarian.bfgroup.xyz "cd /home/bfgbarbarian/barbarian.bfgroup.xyz && npm install && mkdir -p tmp && touch tmp/restart.txt"
